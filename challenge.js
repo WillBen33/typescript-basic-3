ee5a5f796d11c6d6921feb74726017e4693770f2
Will Ben <wilfrid.benoit@outlook.fr>
2023-04-18 09:31:35 +0200

    first commit with typescript challenge solution

---
+++ challenge.js
@@ -0,0 +1,17 @@
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person.prototype.tellMyName = function (name) {
+        return "I am " + this.name;
+    };
+    Person.prototype.tellMyAge = function (age) {
+        return "I am " + this.age + " years old";
+    };
+    return Person;
+}());
+var john = new Person("John", 40);
+var mary = new Person("Mary", 35);
+console.log(john.name, john.age);
+console.log(mary.name, mary.age);
---
+++ challenge.ts
@@ -0,0 +1,24 @@
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public tellMyName(name){
+        return "I am " + this.name;
+    }
+
+    public tellMyAge(age) {
+        return "I am " + this.age + " years old";
+    }
+}
+
+let john = new Person("John", 40);
+let mary = new Person("Mary", 35);
+
+console.log(john.name, john.age);
+console.log(mary.name, mary.age);
+
